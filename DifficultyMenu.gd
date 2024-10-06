4203ed6be02a6cc25ed63057b6c75213eb4a4136
NayWeird <konradneuvirt@seznam.cz>
2024-10-06 14:48:03 +0200

    added main menu & difficulty menu

---
+++ DifficultyMenu.gd
@@ -0,0 +1,34 @@
+extends Control
+@onready var difficulty_menu: Control = $"."
+@onready var easy: Button = $CenterContainer/VBoxContainer/ButtonContainer/Easy
+@onready var hard: Button = $CenterContainer/VBoxContainer/ButtonContainer/Hard
+@onready var close: Button = $CenterContainer/VBoxContainer/Close
+@onready var main_menu: Control = $"../MainMenu"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	difficulty_menu.visible = false
+	easy.pressed.connect(self.easy_picked)
+	hard.pressed.connect(self.hard_picked)
+	close.pressed.connect(self.close_window)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func easy_picked():
+	print("Picked easy difficulty")
+	difficulty_menu.visible = false
+	get_tree().set_pause(false)
+	
+func hard_picked():
+	print("Picked hard difficulty")
+	difficulty_menu.visible = false
+	get_tree().set_pause(false)
+
+func close_window():
+	difficulty_menu.visible = false
+	main_menu.visible = true
---
+++ MainMenu.gd
@@ -0,0 +1,27 @@
+extends Control
+@onready var start: Button = $CenterContainer/VBoxContainer3/ButtonContainer/Start
+@onready var exit: Button = $CenterContainer/VBoxContainer3/ButtonContainer/Exit
+@onready var main_menu: Control = $"."
+@onready var difficulty_menu: Control = $"../DifficultyMenu"
+@onready var timer: Timer = $"../Timer"
+@onready var label: Label = $CenterContainer/VBoxContainer3/LabelContainer/MarginContainer/Label
+var i = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	main_menu.visible = true
+	start.pressed.connect(self.start_game)
+	exit.pressed.connect(self.exit_game)
+	get_tree().set_pause(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func start_game():
+	main_menu.visible = false
+	difficulty_menu.visible = true
+
+func exit_game():
+	get_tree().quit()
